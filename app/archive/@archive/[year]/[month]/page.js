299961ca2112ac9345239216edcc5539c5d74570
Antti Kautiainen <antti@kautiainen.com>
2024-07-05 22:43:35 +0300

    feat: archive page for archive of a month of year modified:   app/archive/@archive/[year]/[month]/page.js - feat: archive page logic for showing archived articles on month of year.

--- app/archive/@archive/[year]/[month]/page.js
+++ app/archive/@archive/[year]/[month]/page.js
@@ -1,13 +1,19 @@
+import LinkListComponent from "@/components/LinkListComponent.mjs";
+import Title from "@/components/title.mjs";
+import { getNewsForYearAndMonth } from "@/lib/dummyNews.mjs";
+
 /**
  * The page for showing the archived messages from a month of a year..
  * @param {*} param0 
  * @returns {React.Component}
  */
-export default function ArchiveYearListPage({params}) {
-    const year = params.year;
-    const month = params.month;
+export default async function ArchiveYearListPage({params}) {
+    const year = Number.parseInt(params.year);
+    const month = Number.parseInt(params.month);
+
+    const news = await getNewsForYearAndMonth(year, month);
 
     return (<section>
-        <header><Title>News Arichive on {month} of {year}</Title></header>
+        <LinkListComponent title={(<>News Arrhive on {month}/{year}</>)} items={news}/>
     </section>)
 }
