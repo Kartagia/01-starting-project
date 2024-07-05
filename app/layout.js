5642270016bbc3cbb2a44118c7f5f1062e100121
Antti Kautiainen <antti@kautiainen.com>
2024-07-05 13:48:22 +0300

    feat: removing main header styling from layout

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
  return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div id="page">
+          <MainMenuComponent/>
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
