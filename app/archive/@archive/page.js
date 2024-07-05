9381dd22f569f8e8d5488e0ba8faa84c8f840d70
Antti Kautiainen <antti@kautiainen.com>
2024-07-05 15:31:53 +0300

    feat: links to the years with archived news modified:   app/archive/@archive/page.js - feat: The navigation link list for years with archived news.

---
+++ app/archive/@archive/page.js
@@ -0,0 +1,26 @@
+import Title from "@/components/title.mjs";
+import { getAvailableNewsYears } from "@/lib/dummyNews.mjs";
+import Link from "next/link";
+
+/**
+ * The archive page with selecting archived news by year and month.
+ *
+ */
+export default function ArchivePage() {
+  const newsYears = getAvailableNewsYears();
+
+  return (
+    <header>
+      <nav>
+        <ul>
+          {newsYears.map((link) => (
+            <li key={link}>
+              <Link href={`/archive/${link}`}>{link}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <Title>Archive Page</Title>
+    </header>
+  );
+}
