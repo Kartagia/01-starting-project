3b59cf69712d62262f86d416536fb9f5113e3cd0
Antti Kautiainen <antti@kautiainen.com>
2024-07-05 14:59:43 +0300

    feat: archive page stub

---
+++ app/archive/@archive/page.js
@@ -0,0 +1,10 @@
+import Title from "@/components/title.mjs";
+
+/**
+ * The archive page with selecting archived news by year and month.
+ * 
+ */
+export default function ArchivePage() {
+
+    return (<Title>Archive Page</Title>)
+}
