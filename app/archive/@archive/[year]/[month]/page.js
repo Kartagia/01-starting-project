7dca53b62cfad87f65d0d96fb32e144cb95ffd6e
Antti Kautiainen <antti@kautiainen.com>
2024-07-05 15:34:14 +0300

    feat: message archive on specific month of a year. new file:   app/archive/@archive/[year]/[month]/page.js - feat: show the title with selected month and year.

---
+++ app/archive/@archive/[year]/[month]/page.js
@@ -0,0 +1,13 @@
+/**
+ * The page for showing the archived messages from a month of a year..
+ * @param {*} param0 
+ * @returns {React.Component}
+ */
+export default function ArchiveYearListPage({params}) {
+    const year = params.year;
+    const month = params.month;
+
+    return (<section>
+        <header><Title>News Arichive on ${month} of ${year}</Title></header>
+    </section>)
+}
