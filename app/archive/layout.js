b6e6660877f3f92ca05b15af8db7a62c1b927a36
Antti Kautiainen <antti@kautiainen.com>
2024-07-05 15:06:39 +0300

    feat: archive layout new file:   app/archive/layout.js - feat: the archive layout with archive and latest parallel routes

---
+++ app/archive/layout.js
@@ -0,0 +1,19 @@
+import Title from "@/components/title.mjs";
+
+/**
+ * The layout containing both latest news page and latest pages.
+ * @returns {React.Component}
+ */
+export default function ArchiveLayout({ archive, latest, children }) {
+  return (
+    <section>
+      <header>
+        <Title>News Archive</Title>
+      </header>
+      <main>
+        <section id="archive-filter">{archive}</section>
+        <secton id="archive-latest">{latest}</secton>
+      </main>
+    </section>
+  );
+}
