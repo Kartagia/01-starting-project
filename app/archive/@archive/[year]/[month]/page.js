7b4b63b6767fa0502a67af9695ade9d9d140b369
Antti Kautiainen <antti@kautiainen.com>
2024-07-05 15:34:14 +0300

    feat: message archive on specific month of a year. new file:   app/archive/@archive/[year]/[month]/page.js - feat: show the title with selected month and year.

--- app/archive/@archive/[year]/[month]/page.js
+++ app/archive/@archive/[year]/[month]/page.js
@@ -1,19 +1,13 @@
-import LinkListComponent from "@/components/LinkListComponent.mjs";
-import Title from "@/components/title.mjs";
-import { getNewsForYearAndMonth } from "@/lib/dummyNews.mjs";
-
 /**
  * The page for showing the archived messages from a month of a year..
  * @param {*} param0 
  * @returns {React.Component}
  */
-export default async function ArchiveYearListPage({params}) {
-    const year = Number.parseInt(params.year);
-    const month = Number.parseInt(params.month);
-
-    const news = await getNewsForYearAndMonth(year, month);
+export default function ArchiveYearListPage({params}) {
+    const year = params.year;
+    const month = params.month;
 
     return (<section>
-        <LinkListComponent title={(<>News Arrhive on {month}/{year}</>)} items={news}/>
+        <header><Title>News Arichive on ${month} of ${year}</Title></header>
     </section>)
 }
