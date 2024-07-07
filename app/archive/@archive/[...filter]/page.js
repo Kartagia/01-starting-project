98c315b7d06941bf6236639839e8ed3c3cedecea
Antti Kautiainen <antti@kautiainen.com>
2024-07-07 12:27:09 +0300

    feat: month page implementation with catch-all Changes: modified:   app/archive/@archive/[...filter]/page.js - feat: month route - feat: checkYear for checking validity of year. - feat: checkMonth for checking validity of month. - fix: routing appends to old links. - fix: routing appends to old messages.

--- app/archive/@archive/[...filter]/page.js
+++ app/archive/@archive/[...filter]/page.js
@@ -4,6 +4,7 @@ import {
   getAvailableNewsMonths,
   getAvailableNewsYears,
   getNewsForYear,
+  getNewsForYearAndMonth,
 } from "@/lib/dummyNews.mjs";
 import Link from "next/link";
 
@@ -29,42 +30,60 @@ export default async function ArchivePage({ params }) {
    */
   let news = [];
 
-  // The year is given.
-  if (selectedYear && !selectedMonth) {
-    // The Year is selected.
-    const year = +selectedYear;
-    if (Number.isSafeInteger(year)) {
-      // The year is okay.
-      const loadedNews = await getNewsForYear(year);
-      news.push(...loadedNews);
-      const availableMonths = await getAvailableNewsMonths(year);
-      links.push(
-        ...availableMonths.map((month) => ({
-          key: month,
-          href: `${year}/${month}`,
-          title: `${month}`,
-          toString() {
-            return this.title;
-          },
-        }))
-      );
+  function checkInteger(value, message = `The value must be an integer`) {
+    const result = +value;
+    if (Number.isSafeInteger(result)) {
+      return result;
     } else {
-      // The year is not okay.
-      throw new RangeError(`The year must be an integer`);
+      throw new RangeError(nessage);
     }
+  }
+
+  function checkYear(year) {
+    return checkInteger(year, "The year must be an integer");
+  }
+  function checkMonth(month) {
+    const result = checkInteger(month, "The month must be an integer");
+    if (result < 1 || result > 12) {
+      throw new RangeError("The month of year must be from 1 to 12");
+    } else {
+      return result;
+    }
+  }
+
+  // The year is given.
+  if (selectedYear && selectedMonth) {
+    // Tye year and month is given.
+    const year = checkYear(selectedYear);
+    const month = checkMonth(selectedMonth);
+    links = [];
+    news = await getNewsForYearAndMonth(year, month);
+  } else if (selectedYear && !selectedMonth) {
+    // The Year is selected.
+    const year = checkYear(selectedYear);
+
+    // The year is okay.
+    news  = await getNewsForYear(year);
+    const availableMonths = await getAvailableNewsMonths(year);
+    links = availableMonths.map((month) => ({
+        key: month,
+        href: `${year}/${month}`,
+        title: `${month}`,
+        toString() {
+          return this.title;
+        },
+      }));
   } else {
     // The year is not given - creating the available years.
     const years = await getAvailableNewsYears();
-    links.push(
-      ...years.map((year) => ({
+    links = years.map((year) => ({
         key: year,
         href: `/archive/${year}`,
         title: `${year}`,
         toString() {
           return this.title;
         },
-      }))
-    );
+      }));
   }
 
   const content = news.length ? (
