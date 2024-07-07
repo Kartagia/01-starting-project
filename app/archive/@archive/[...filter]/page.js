8c87b60d0da8e8b50a2b778bfe994f9a7a0f62c2
Antti Kautiainen <antti@kautiainen.com>
2024-07-07 12:13:44 +0300

    feat: year page implementation modified:   app/archive/@archive/[...filter]/page.js - feat: The default news archive with links to the available years. - feat: The show year archive.

--- app/archive/@archive/[...filter]/page.js
+++ app/archive/@archive/[...filter]/page.js
@@ -1,11 +1,24 @@
 import LinkListComponent from "@/components/LinkListComponent.mjs";
 import Title from "@/components/title.mjs";
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+} from "@/lib/dummyNews.mjs";
 import Link from "next/link";
 
 /**
  * The catch all implementation of the archive page.
  */
-export default function ArchivePage() {
+export default async function ArchivePage({ params }) {
+  // Getting the parameters.
+  const filter = params.filter;
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
+
+  console.log(`Selected year: ${selectedYear ?? "No year selected"}`);
+  console.log(`Selected month: ${selectedMonth ?? "No month selected"}`);
+
   /**
    * The links of the subcontext.
    */
@@ -15,6 +28,44 @@ export default function ArchivePage() {
    * The shown news articles.
    */
   let news = [];
+
+  // The year is given.
+  if (selectedYear && !selectedMonth) {
+    // The Year is selected.
+    const year = +selectedYear;
+    if (Number.isSafeInteger(year)) {
+      // The year is okay.
+      const loadedNews = await getNewsForYear(year);
+      news.push(...loadedNews);
+      const availableMonths = await getAvailableNewsMonths(year);
+      links.push(
+        ...availableMonths.map((month) => ({
+          key: month,
+          href: `${year}/${month}`,
+          title: `${month}`,
+          toString() {
+            return this.title;
+          },
+        }))
+      );
+    } else {
+      // The year is not okay.
+      throw new RangeError(`The year must be an integer`);
+    }
+  } else {
+    // The year is not given - creating the available years.
+    const years = await getAvailableNewsYears();
+    links.push(
+      ...years.map((year) => ({
+        key: year,
+        href: `/archive/${year}`,
+        title: `${year}`,
+        toString() {
+          return this.title;
+        },
+      }))
+    );
+  }
 
   const content = news.length ? (
     <LinkListComponent items={news} />
@@ -28,8 +79,8 @@ export default function ArchivePage() {
         <nav>
           <ul>
             {links.map((link) => (
-              <li key={link}>
-                <Link href={`/archive/${link}`}>{link}</Link>
+              <li key={link.key}>
+                <Link href={`/archive/${link.href}`}>{`${link}`}</Link>
               </li>
             ))}
           </ul>
