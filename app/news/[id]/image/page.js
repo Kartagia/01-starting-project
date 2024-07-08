25c2ae2baeaf85cc1662e476f7fb5ca1c5708ab6
Antti Kautiainen <antti@kautiainen.com>
2024-07-08 15:11:43 +0300

    feat: use LinkListComponent to show the link.
    modified:   app/news/[id]/image/page.js
    - feat: Link list component page uses LinkListComponent

--- app/news/[id]/image/page.js
+++ app/news/[id]/image/page.js
@@ -1,16 +1,17 @@
+import NewsLinkComponent from "@/components/NewsLinkComponent.mjs";
 import { getPost } from "@/lib/dummyNews.mjs";
 import { notFound } from "next/navigation";
 
 /**
  * The page showing full screen image.
  */
-export default async function IamgePage({params}) {
+export default async function ImagePage({params}) {
 
     const newsItemId = params.id;
     try {
     const newsItem = await getPost(newsItemId);
     return (<div className={"fullscreen-iamge"}>
-        <img src={`/images/news/${newsItemId}`} alt={newsItem.title}/>
+        <NewsLinkComponent newsId={newsItemId} newsItem={newsItem} />
     </div>)
     } catch(err) {
         notFound();
