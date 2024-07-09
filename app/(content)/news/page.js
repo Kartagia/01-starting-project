6d6716777fccd367c349f5162c0c6bac21573ec0
Antti Kautiainen <antti@kautiainen.com>
2024-07-09 11:32:35 +0300

    feat: NewsPage using loaded news.
    The update to the NewsPage to get the news data from the promise
    with loading. The dummy data is still used.

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,15 +1,45 @@
+"use client";
+
 import LinkListComponent from "@/components/LinkListComponent.mjs";
 import Title from "@/components/title.mjs";
 import { getAllPosts } from "@/lib/dummyNews.mjs";
 
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 
 /**
  * The page showing all meals. 
  */
-export default async function NewsListPage() {
-  const news = await getAllPosts();
+export default function NewsListPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [news, setNews] = useState([]);
+  const [error, setError] = useState();
+  useEffect(() => {
+    /**
+     * Load all news and update the state accordingly.
+     */
+    async function fetchNews() {
+      setIsLoading(true);
+      getAllPosts().then( (entries) => {
+        setNews(entries);
+        setIsLoading(false);
+      }, (error) => {
+        setError(error.message);
+        setIsLoading(false);
+      })
+    }
+    // Fetching the news.
+    fetchNews();
+  }, []);
+
+  if (isLoading) {
+
+  }
+
+  if (error) {
+    return (<p>{error.message}</p>)
+  }
 
   return (
     <section>
