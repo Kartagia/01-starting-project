95245ebc3db976377dc9e2036bd70929499b7e54
Antti Kautiainen <antti@kautiainen.com>
2024-07-09 17:49:38 +0300

    feat: fullscreen image page converted to server
    The full screen image component to server side.
    modified:   app/(content)/news/[id]/image/page.js
    - refactor: Full screen image page changed to server component.

--- app/(content)/news/[id]/image/page.js
+++ app/(content)/news/[id]/image/page.js
@@ -1,27 +1,44 @@
 "use client";
-import NewsLinkComponent from "@/components/NewsLinkComponent.mjs";
-import Title from "@/components/title.mjs";
-import { getPost } from "@/lib/dummyNews.mjs";
+import { getNewsArticle } from "@/lib/BackendNewsService.mjs";
 import Link from "next/link";
 import { notFound, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 /**
  * The page showing full screen image.
  */
-export default async function ImagePage({ params }) {
+export default function ImagePage({ params }) {
+    const [loading, setLoading] = useState(true);
+    const [newsItem, setNewsItem] = useState();
+    const [error, setError] = useState();
+    useEffect(() => {
+        setLoading(true);
+        async function getNewsItem(id) {
+            setNewsItem(await getNewsArticle(id));
+            setLoading(false);
+        }
+
+        getNewsItem(params.id);
+    }, []);
+
+    if (error) {
+        throw Error("Could not load the image");
+    }
+
     const router = new useRouter();
+
+    const content = (loading ? (<p>Loading...</p>) : (<>
+        <div className={"modal-backdrop"} onClick={router.back} />
+        <dialog className={"modal"} open>
+            <div className={"fullscreen-image"}>
+                <img width="100%" src={`/images/news/${newsItem.image}`} alt={`${newsItem.title}`} />
+            </div>
+        </dialog>
+    </>));
 
     const newsItemId = params.id;
     try {
-        const newsItem = await getPost(newsItemId);
-        return (<>
-            <div className={"modal-backdrop"} onClick={router.back} />
-            <dialog className={"modal"} open>
-                <div className={"fullscreen-image"}>
-                    <img width="100%" src={`/images/news/${newsItem.image}`} alt={`${newsItem.title}`} />
-                </div>
-            </dialog>
-        </>)
+        return <>{content}</>;
     } catch (err) {
         console.log(err);
         notFound();
