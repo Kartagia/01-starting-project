e9dd0623d13c13f43dce360c499c27a379f1a74b
Antti Kautiainen <antti@kautiainen.com>
2024-07-09 07:08:41 +0300

    feat: full screen image as a dialog working
    modified:   app/news/[id]/image/page.js
    - feat: Show FullScreen image as a dialog.
      - fix: MIssing global.css style for fullscreen-image circumvented
      with property width=100%.

--- app/news/[id]/image/page.js
+++ app/news/[id]/image/page.js
@@ -1,19 +1,29 @@
+"use client";
 import NewsLinkComponent from "@/components/NewsLinkComponent.mjs";
+import Title from "@/components/title.mjs";
 import { getPost } from "@/lib/dummyNews.mjs";
-import { notFound } from "next/navigation";
+import Link from "next/link";
+import { notFound, useRouter } from "next/navigation";
 
 /**
  * The page showing full screen image.
  */
-export default async function ImagePage({params}) {
+export default async function ImagePage({ params }) {
+    const router = new useRouter();
 
     const newsItemId = params.id;
     try {
-    const newsItem = await getPost(newsItemId);
-    return (<div className={"fullscreen-image"}>
-        <NewsLinkComponent imageLink={"article"} newsId={newsItemId} newsItem={newsItem} />
-    </div>)
-    } catch(err) {
+        const newsItem = await getPost(newsItemId);
+        return (<>
+            <div className={"modal-backdrop"} onClick={router.back} />
+            <dialog className={"modal"} open>
+                <div className={"fullscreen-image"}>
+                    <img width="100%" src={`/images/news/${newsItem.image}`} alt={`${newsItem.title}`} />
+                </div>
+            </dialog>
+        </>)
+    } catch (err) {
+        console.log(err);
         notFound();
     }
 }
